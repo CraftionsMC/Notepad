fc4f183a83987364595496e6e0a7b404572a9e1d
MCTzOCK <dev@mctzock.de>
2021-02-14 17:44:46 +0100

    First commit!

---
+++ src/main/java/net/craftions/notepad/main/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.notepad.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
